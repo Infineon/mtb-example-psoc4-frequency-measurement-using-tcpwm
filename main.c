96642cee6e8ffe7a331988252c8e25dda633f1ec
gitlab-runner <do-not-reply@cypress.com>
2026-03-26 19:39:33 -0500

    Upload mtb-example-psoc4-frequency-measurement-using-tcpwm [4724]

--- main.c
+++ main.c
@@ -1,69 +1,66 @@
 /******************************************************************************
  * File Name: main.c
  *
- * Description: This is the source code for the PSoC 4 Frequency measurement
+ * Description: This is the source code for the PSOC 4 Frequency measurement
  * using TCPWM Application for ModusToolbox.
  *
- * Related Document: See README.md 
+ * Related Document: See README.md
  *
  *
- *******************************************************************************
- * Copyright 2023-2025, Cypress Semiconductor Corporation (an Infineon company) or
- * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+ ******************************************************************************
+ * (c) 2023-2026, Infineon Technologies AG, or an affiliate of Infineon
+ * Technologies AG. All rights reserved.
+ * This software, associated documentation and materials ("Software") is
+ * owned by Infineon Technologies AG or one of its affiliates ("Infineon")
+ * and is protected by and subject to worldwide patent protection, worldwide
+ * copyright laws, and international treaty provisions. Therefore, you may use
+ * this Software only as provided in the license agreement accompanying the
+ * software package from which you obtained this Software. If no license
+ * agreement applies, then any use, reproduction, modification, translation, or
+ * compilation of this Software is prohibited without the express written
+ * permission of Infineon.
  *
- * This software, including source code, documentation and related
- * materials ("Software") is owned by Cypress Semiconductor Corporation
- * or one of its affiliates ("Cypress") and is protected by and subject to
- * worldwide patent protection (United States and foreign),
- * United States copyright laws and international treaty provisions.
- * Therefore, you may use this Software only as provided in the license
- * agreement accompanying the software package from which you
- * obtained this Software ("EULA").
- * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
- * non-transferable license to copy, modify, and compile the Software
- * source code solely for use in connection with Cypress's
- * integrated circuit products.  Any reproduction, modification, translation,
- * compilation, or representation of this Software except as specified
- * above is prohibited without the express written permission of Cypress.
- *
- * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
- * reserves the right to make changes to the Software without notice. Cypress
- * does not assume any liability arising out of the application or use of the
- * Software or any product or circuit described in the Software. Cypress does
- * not authorize its products for use in any products where a malfunction or
- * failure of the Cypress product may reasonably be expected to result in
- * significant property damage, injury or death ("High Risk Product"). By
- * including Cypress's product in a High Risk Product, the manufacturer
- * of such system or application assumes all risk of such use and in doing
- * so agrees to indemnify Cypress against all liability.
- *******************************************************************************/
+ * Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
+ * IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
+ * THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
+ * SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
+ * Infineon reserves the right to make changes to the Software without notice.
+ * You are responsible for properly designing, programming, and testing the
+ * functionality and safety of your intended application of the Software, as
+ * well as complying with any legal requirements related to its use. Infineon
+ * does not guarantee that the Software will be free from intrusion, data theft
+ * or loss, or other breaches ("Security Breaches"), and Infineon shall have
+ * no liability arising out of any Security Breaches. Unless otherwise
+ * explicitly approved by Infineon, the Software may not be used in any
+ * application where a failure of the Product or any consequences of the use
+ * thereof can reasonably be expected to result in personal injury.
+ *****************************************************************************/
 
-/*******************************************************************************
+/******************************************************************************
  * Include header files
- ********************************************************************************/
+ *****************************************************************************/
 #include "cy_pdl.h"
 #include "cybsp.h"
 #include <stdio.h>
 
-/*******************************************************************************
+/******************************************************************************
  * Macros and Constants
- ********************************************************************************/
+ *****************************************************************************/
 #define SET                         (1u)
 #define RESET                       (!SET)
 #define BUFFER_SIZE                 (128u)
-#define MAX_COUNT                   (65536u)    /* 16 bit Counter */
+#define MAX_COUNT                   (COUNTER_config.period) /* 16 bit Counter */
 #define CY_ASSERT_FAILED            (0u)
 
-/*******************************************************************************
+/******************************************************************************
  * Function Prototypes
- ********************************************************************************/
+ *****************************************************************************/
 static void isr_counter(void);
 
-/*******************************************************************************
+/******************************************************************************
  * Global Variables
- ********************************************************************************/
+ *****************************************************************************/
 cy_stc_scb_uart_context_t uart_context;
 volatile uint8_t int_flag = RESET;
 volatile uint32_t counter_overflow = RESET;
@@ -71,29 +68,29 @@ char buffer[BUFFER_SIZE];
 
 /******************************************************************************
  * Switch interrupt configuration structure
- *******************************************************************************/
+ *****************************************************************************/
 const cy_stc_sysint_t sysint_counter_cfg =
-{
-    /*.intrSrc =*/ COUNTER_IRQ,   /* Interrupt source */
-    /*.intrPriority =*/ 3UL       /* Interrupt priority is 3 */
+    {
+        /*.intrSrc =*/COUNTER_IRQ, /* Interrupt source */
+        /*.intrPriority =*/3UL     /* Interrupt priority is 3 */
 };
 
-/*******************************************************************************
+/******************************************************************************
  * Function Name: main
- ********************************************************************************
+ ******************************************************************************
  * Summary:
  * System entrance point. This function performs
  *  1. Initializes the BSP.
  *  2. This function initializes the peripherals to measure the frequency
  *     and display the result accordingly.
- * 
+ *
  * Parameters:
  *  None
  *
  * Return:
  *  int
  *
- *******************************************************************************/
+ *****************************************************************************/
 int main(void)
 {
     cy_rslt_t result = CY_RSLT_SUCCESS;
@@ -124,7 +121,7 @@ int main(void)
     /* Initialize Counter with config set in Peripheral */
     if (CY_TCPWM_SUCCESS != Cy_TCPWM_Counter_Init(COUNTER_HW, COUNTER_NUM, &COUNTER_config))
     {
-        CY_ASSERT (CY_ASSERT_FAILED);
+        CY_ASSERT(CY_ASSERT_FAILED);
     }
 
     /* Hook up and enable interrupt */
@@ -138,7 +135,7 @@ int main(void)
     NVIC_EnableIRQ(sysint_counter_cfg.intrSrc);
 
     /* Initialize OneSecTimer with config set in Peripheral */
-    if (CY_TCPWM_SUCCESS !=Cy_TCPWM_Counter_Init(ONE_SEC_TIMER_HW, ONE_SEC_TIMER_NUM, &ONE_SEC_TIMER_config))
+    if (CY_TCPWM_SUCCESS != Cy_TCPWM_Counter_Init(ONE_SEC_TIMER_HW, ONE_SEC_TIMER_NUM, &ONE_SEC_TIMER_config))
     {
         CY_ASSERT(CY_ASSERT_FAILED);
     }
@@ -166,11 +163,11 @@ int main(void)
     /* \x1b[2J\x1b[;H - ANSI ESC sequence to clear screen */
     Cy_SCB_UART_PutString(UART_HW, "\x1b[2J\x1b[;H");
     /* Display code example title */
-    Cy_SCB_UART_PutString(UART_HW, "CE236702 - PSoC 4 - Frequency measurement using TCPWM\n\r");
+    Cy_SCB_UART_PutString(UART_HW, "CE236702 - PSOC 4 - Frequency measurement using TCPWM\n\r");
 
     for (;;)
     {
-        if (int_flag)    /* Interrupt was triggered, read the captured counter value now */
+        if (int_flag) /* Interrupt was triggered, read the captured counter value now */
         {
             /* Get the Counter value */
             capture_val = Cy_TCPWM_Counter_GetCapture(COUNTER_HW, COUNTER_NUM);
@@ -190,9 +187,9 @@ int main(void)
     }
 }
 
-/*******************************************************************************
+/******************************************************************************
  * Function Name: isr_counter
- ********************************************************************************
+ ******************************************************************************
  * Summary: Interrupt service routine for Counter interrupt. This function is
  * executed when a capture or an overflow event happens.
  *
@@ -201,7 +198,7 @@ int main(void)
  *
  * Return:
  *  None
- *******************************************************************************/
+ *****************************************************************************/
 static void isr_counter(void)
 {
     /* Get which event triggered the interrupt */
@@ -232,6 +229,5 @@ static void isr_counter(void)
         overflow_count++;
     }
 }
-
 
 /* [] END OF FILE */
